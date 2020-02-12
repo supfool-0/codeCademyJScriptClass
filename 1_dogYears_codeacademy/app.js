3cd399a97963e6d486d8f6c0fb6aa249c1f39657
jeri <diltsjeri@gmail.com>
2020-02-12 18:58:22 -0500

    add dogYears dir w/code

---
+++ 1_dogYears_codeacademy/app.js
@@ -0,0 +1,27 @@
+// assigning my age to a variable
+let myAge = 30;
+
+// first two years of a dogs life
+let earlyYears = 2;
+
+// fancy. using the assignment operator
+earlyYears *= 10.5;
+
+// my age minus 2
+let laterYears = myAge - 2;
+
+// fancy again!
+laterYears *= 4;
+
+// just checking
+//console.log(`${laterYears}`);
+//console.log(`${earlyYears}`);
+
+let myAgeInDogYears = earlyYears + laterYears;
+
+//making my name lowercase
+let myName = 'Jeri';
+myName = myName.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+
