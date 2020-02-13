58a10e8b24fb4f5334de91fe659c372bbcb96df8
jeri <diltsjeri@gmail.com>
2020-02-12 19:05:17 -0500

    magicEightBall code  :)

---
+++ 2_magicEightBall/app.js
@@ -0,0 +1,37 @@
+// input name
+let userName = 'Jeri';
+
+// practice using ternary, so cool
+userName !== '' ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
+
+// user question
+let userQuestion = 'How cute is my dog?';
+
+// print question to the screen
+console.log(`${userName} asks: ${userQuestion}`);
+
+// creating a random number
+let randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = '';
+
+if(randomNumber = 0){
+  console.log('It is certain');
+} else if (randomNumber = 1){
+  console.log('It is decidedly so');
+} else if (randomNumber = 2){
+  console.log('Reply hazy try again');
+} else if (randomNumber = 3){
+  console.log('Cannot predict now');
+} else if (randomNumber = 4){
+  console.log('Do not count on it');
+} else if (randomNumber = 5){
+  console.log('My sources say no');
+} else if (randomNumber = 6){
+  console.log('Outlook not so good');
+} else if (randomNumber = 7){
+  console.log('Signs point to yes');
+}
+
+
+console.log(eightBall);
